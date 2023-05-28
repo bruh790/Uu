252fd80b6421aae0b155724c08b1ce2fa2efe9cb
bruh790 <133423002+bruh790@users.noreply.github.com>
2023-05-28 20:00:59 +0300

    Create uuuu.py
    E

---
+++ uuuu.py
@@ -0,0 +1,14 @@
+ import random
+a="azertyuiopqsdfghjklmwxcvbn"
+b="AZERTYUIOPQSDFGHJKLMWXCVBN"
+n="1234567890"
+s="@§%&'(--+/*="
+w=8
+g=a+b+s+n
+x=0
+while True:
+  pas="".join(random.sample(g,w))
+  print("password is: ",pas)
+  x+=1
+  
+  if x==100000000000000
